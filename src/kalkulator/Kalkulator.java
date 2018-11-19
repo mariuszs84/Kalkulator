aa36292c06c2839909108ee38d2bc01daa9c873f
Mariusz <s.mariusz@interia.pl>
2018-11-19 19:51:27 +0100

    Edycja potęgowania
    Signed-off-by: Mariusz <s.mariusz@interia.pl>

--- src/kalkulator/Kalkulator.java
+++ src/kalkulator/Kalkulator.java
@@ -15,6 +15,7 @@ public class Kalkulator {
         System.out.print("Podaj druga liczbe:\t\t");
         double b = scanner.nextDouble();
         System.out.println("\t\t\t\t=");
+        double c=1;
 
         if (znak.equals("+")) {
             System.out.println("\t\t\t\t" + (a + b));
@@ -25,7 +26,10 @@ public class Kalkulator {
         } else if (znak.equals("/")) {
             System.out.println("\t\t\t\t" + (a / b));
         } else if (znak.equals("^")) {
-            System.out.println("\t\t\t\t" + (pow(a, b)));
+            for(int i=0;i<b;i++){
+                c*=a;
+            }
+            System.out.println("\t\t\t\t"+c);
         } else {
             System.out.println("Operacja niedozwolona");
         }
